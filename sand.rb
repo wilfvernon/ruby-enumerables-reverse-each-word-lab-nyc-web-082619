6ca81ed27a0cabe1eb6c97eced6fb3ad439007a7
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-22 17:05:12 +0000

    Automatically backed up by Learn

--- sand.rb
+++ sand.rb
@@ -4,7 +4,7 @@ def reverse_each_word(string)
   puts array
   reverse = Array.new
   array.each {|w| reverse.unshift(w.reverse)}
-  puts reverse
+  puts reverse.class
 end
 
 reverse_each_word("Is this an array?")
