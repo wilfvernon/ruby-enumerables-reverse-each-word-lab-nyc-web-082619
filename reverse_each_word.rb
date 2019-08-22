836a256dc980a33756984e13b168f2177c0fd162
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-22 17:11:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,9 @@ def reverse_each_word(string)
   p reverse.join(" ")
 end
 
+def reverse_each_word(string)
+  array = string.split
+  reverse = Array.new
+  array.map {|w| reverse.push(w.reverse)}
+  p reverse.join(" ")
+end
