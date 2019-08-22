c25e23154190e89951a15975a0814a33ad741ae7
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-22 17:12:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,4 @@
-def reverse_each_word(string)
-  array = string.split
-  reverse = Array.new
-  array.each {|w| reverse.push(w.reverse)}
-  p reverse.join(" ")
-end
+
 
 def reverse_each_word(string)
   array = string.split
